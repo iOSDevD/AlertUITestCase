87c3569be31ddc0bc488f2d95922b739edc11753
iOSDev <iosdev@Nikunjs-MacBook-Pro.local>
2019-08-23 23:36:42 -0400

    First Commit

---
+++ AlertUITestcase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertUITestcase
+//
+//  Created by iOSDev on 8/23/19.
+//  Copyright © 2019 iOSDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
